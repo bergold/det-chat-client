cadd824099273e36fe45a69cdff1bfc59355e7bc
bergold <emil.bergold@outlook.com>
2014-03-23 22:40:03 +0100

    testing api, auth and user services

--- js/app/controller.js
+++ js/app/controller.js
@@ -8,19 +8,66 @@
  */
 
 
-chat.controller('TestCtrl', ['$scope', 'api', 'auth', function($scope, api, auth) {
+chat.controller('TestCtrl', ['$scope', 'api', 'auth', 'user', function($scope, api, auth, user) {
     
-    $scope.validapi = false;
+    $scope.tests = [];
     
+    var test = function(name, res) {
+        console.log("Test : ", name + "(" + $scope.tests.length + ")", res)
+        return $scope.tests.push({
+            name: name,
+            result: res
+        }) - 1;
+    };
+    var update = function(i, res) {
+        console.log("Test*: ", i, res);
+        if ($scope.tests[i]) $scope.tests[i].result = res;
+    }
+    
+    var api_test = test('api', 'false');
     api().then(function(a) {
-        $scope.validapi = true;
-        
-        auth.login("emil", "bergie").then(function() {
-            console.log("success", arguments);
-        },
-        function(reson) {
-            console.log("error", arguments);
-        });
+        update(api_test, true);
     });
+    
+    var login_test = test('login', 'pending');
+    auth.login("emil", "bergie").then(function(sid) {
+        update(login_test, "success " + sid);
+        
+        var user_test_sid = test('user-sid', 'pending');
+        user(sid).then(function(res) {
+            update(user_test_sid, res);
+        });
+        
+        var user_test_ping = test('ping', 'pending');
+        user.ping(sid).then(function(res) {
+            update(user_test_ping, res);
+        }, function(res) {
+            update(user_test_ping, "error: " + res);
+        });
+            
+    },
+    function(reson) {
+        update(login_test, "error " + reson);
+    });
+    
+    var user_test = test('user', 'pending');
+    user('emil').then(function(res) {
+        update(user_test, res);
+    });
+    
+    $scope.testUser = function() {
+        var user_test_2 = test('user', 'pending');
+        user('emil', true).then(function(res) {
+            update(user_test_2, res);
+        });
+    };
+    
+    $scope.testImg = function() {
+        var img_test = test('img', 'pending');
+        user.getImg('emil').then(function(res) {
+            $scope.imgSrc = res;
+            update(img_test, true);
+        });
+    };
     
 }]);
--- js/app/service.js
+++ js/app/service.js
@@ -8,6 +8,10 @@
  */
 
 
+// definitions
+chat.constant('online_tolerance', 10);
+
+
 // api-service
 chat.factory('api', ['$q' , function($q) {
     return function() {
@@ -71,14 +75,82 @@ chat.factory('auth', ['$q', 'api', function($q, api) {
 
 
 // user-service
-chat.provider('user', ['$q', 'api', function($q, api) {
+chat.provider('user', function() {
     
-    this.buffer_ = [];
+    this.buffer_ = {};
+    this.imgbuffer_ = {};
     
-    this.$get = function() {
-        return {
-            
-        }
-    };
+    this.$get = ['$q', 'api', 'online_tolerance', function($q, api, online_tolerance) {
+        var self = this;
+        var coreuser_ = function(name, force) {
+            force = force || false;
+            var defered = $q.defer();
+            if (!force && self.buffer_[name])
+                defered.resolve(self.buffer_[name]);
+            else api().then(function(req) {
+                req({
+                    error: function(res, xhr) {
+                        defered.reject(xhr);
+                    }
+                }).cmd({
+                    cmd: 'getdata',
+                    id: name,
+                    getimage: false
+                }, function(res) {
+                    if (false===res)
+                        defered.reject(false);
+                    else {
+                        self.buffer_[name] = res;
+                        defered.resolve(res);
+                    }
+                }).send();
+            });
+            return defered.promise;
+        };
+        coreuser_.getImg = function(name) {
+            var defered = $q.defer();
+            if (self.imgbuffer_[name])
+                defered.resolve(self.imgbuffer_[name]);
+            else api().then(function(req) {
+                req({
+                    error: function(res, xhr) {
+                        defered.reject(xhr);
+                    }
+                }).cmd({
+                    cmd: 'getdata',
+                    id: name,
+                    getimage: true
+                }, function(res) {
+                    if (false===res)
+                        defered.reject(false);
+                    else {
+                        self.imgbuffer_[name] = res.image;
+                        defered.resolve(res.image);
+                    }
+                }).send();
+            });
+            return defered.promise;
+        };
+        coreuser_.ping = function(sid) {
+            var defered = $q.defer();
+            api().then(function(req) {
+                req({
+                    error: function(res, xhr) {
+                        defered.reject(xhr);
+                    }
+                }).cmd({
+                    cmd: 'ping',
+                    sid: sid,
+                    tolerance: online_tolerance
+                }, function(res) {
+                    defered.resolve(res[0]);
+                    if (false===res[1])
+                        defered.reject(false);
+                }).send();
+            });
+            return defered.promise;
+        };
+        return coreuser_;
+    }];
     
-}]);
+});
